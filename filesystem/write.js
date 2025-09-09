e294fb9ad918707c9d3f852f0b97dbd68cba4bfe
joystackinfo <joynelsoninfo@gmail.com>
2025-09-09 12:29:01 +0100

    added a new line to write.js

--- filesystem/write.js
+++ filesystem/write.js
@@ -1,5 +1,5 @@
  const fs = require('fs');
- fs.writeFile('output.txt', 'Hello, NodeJs', (err) => {
+ fs.writeFile('output.txt', 'Hello, NodeJs,welcome to the world of file systems!,also node.js ', (err) => {
      if (err) {
         console.error(err);
         return;
