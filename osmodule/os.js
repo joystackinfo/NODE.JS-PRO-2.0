5c22a43a3942371a5c19245d5a50457928074ba2
joystackinfo <joynelsoninfo@gmail.com>
2025-12-15 17:44:10 +0100

    Added comment

--- osmodule/os.js
+++ osmodule/os.js
@@ -1,6 +1,7 @@
 const os = require('os');
 
 console.log('Operating System Information:');
+
 //console.log('--------------------------------',{os});
  console.log('platform:', os.platform());
 console.log('architecture:', os.arch());
